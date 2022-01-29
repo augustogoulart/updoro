f7ff615fd768f47cb04958ad926e000570e17c84
Augusto Goulart <augusto@goulart.me>
2022-01-28 22:27:12 -0300

    Count iterations

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Timer} from "./components/Timer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Timer />
-          <div className={"controls"}>
-              Pause
-          </div>
-          <div className={"intervals"}>
-              1/10
-          </div>
-      </header>
-    </div>
-  );
+    const [time, setTime] = useState(5)
+    const [runs, setRuns] = useState(1)
+    const timer_has_ended = time === 0
+
+    function runTimer() {
+        setTime(time - 1)
+    }
+
+    useEffect(() => {
+        if (timer_has_ended) {
+            setRuns(runs + 1)
+            setTime(5)
+        } else {
+            const timerId = setInterval(runTimer, 1000)
+            return function () {
+                clearInterval(timerId)
+            }
+        }
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {time}
+                <div className={"controls"}>
+                    Pause
+                </div>
+                <div className={"intervals"}>
+                    {runs}/10
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
