0be6a631ffce102d0f98c16cb4b4d4ab09965e50
Augusto Goulart <augusto@goulart.me>
2022-03-29 10:22:29 -0300

    Stop session when timer runs out

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {timerHasEnded ? <TimerEnded/> : <TimerRunner time={time} runs={runs} timers={timers}/>}
+        {runs === timers.length && timerHasEnded ? <TimerEnded/> : <TimerRunner time={time} runs={runs} timers={timers}/>}
       </header>
     </div>
   );
