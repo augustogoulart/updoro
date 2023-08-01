43f5316df348388542e697d12bd37db5e3350254
Augusto Goulart <augusto@goulart.me>
2023-08-01 15:45:45 -0300

    Initial Commit

---
+++ updoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  updoro
+//
+//  Created by Augusto Goulart on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ updoro/updoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  updoroApp.swift
+//  updoro
+//
+//  Created by Augusto Goulart on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct updoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
