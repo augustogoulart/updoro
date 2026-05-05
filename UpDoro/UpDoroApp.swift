e4fe192a500936155bfe8d6aff350c9b13c8efc2
Augusto Goulart <augusto@goulart.me>
2026-05-05 20:57:43 -0300

    Init project

---
+++ UpDoro/UpDoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpDoroApp.swift
+//  UpDoro
+//
+//  Created by Augusto Goulart on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct UpDoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpDoroTests/UpDoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpDoroTests.swift
+//  UpDoroTests
+//
+//  Created by Augusto Goulart on 05/05/26.
+//
+
+import Testing
+@testable import UpDoro
+
+struct UpDoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
